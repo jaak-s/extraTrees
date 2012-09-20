43da4116f9e3159e7c6715b7e91c5d68e50075c6
Jaak Simm <jaak.simm@gmail.com>
2012-09-20 22:04:02 +0300

    improved regression testing

--- R-test/basic-regression.R
+++ R-test/basic-regression.R
@@ -5,10 +5,20 @@ library("extraTrees")
 n = 200
 p = 10
 x = matrix(runif(n*p), n, p)
-y = (x[,1]>0.5) + x[,2] - x[,3] + 0.5*x[,4]
+f = function(x) (x[,1]>0.5) + x[,2] - x[,3] + 0.5*x[,4]
+y = f(x)
+
+xtest = matrix(runif(n*p), n, p)
+ytest = f(xtest)
 
 ## learning extra trees:
-et = extraTrees(x, y, nodesize=5)
+et = extraTrees(x, y, nodesize=3)
 yhat = predict(et, x)
 yerr = mean( (y-yhat)^2 )
+
+ytest_hat = predict(et, xtest)
+ytest_err = mean( (ytest-ytest_hat)^2 )
+
+print( sprintf("ytrain err: %f",yerr) )
+print( sprintf("ytest err:  %f",ytest_err) )
 
