e1578245a2688f877c617463a45980c2d619e7c2
Jaak Simm <jaak.simm@gmail.com>
2012-11-22 12:30:47 +0900

    large scale testing fixed to use new testing samples

--- R-test/large-scale-test.R
+++ R-test/large-scale-test.R
@@ -1,22 +1,25 @@
 library("extraTrees")
 
 ## making data:
-n = 10000
+n = 5000
 p = 4
 x = matrix(runif(n*p), n, p)
 f = function(x) (x[,1]>0.5) + 0.8*(x[,2]>0.6) + 0.5*(x[,3]>0.4)
 y = as.numeric(f(x))
 
+xtest = matrix(runif(n*p), n, p)
+ytest = f(xtest)
+
 ## extraTrees
-time.et = system.time( {et = extraTrees(x, y, nodesize=3, mtry=p)} )
-yhat    = predict(et, x)
-yerr.et = mean( (y-yhat)^2 )
+time.et = system.time( {et = extraTrees(x, y, nodesize=5, mtry=p)} )
+yhat    = predict(et, xtest)
+yerr.et = mean( (ytest-yhat)^2 )
 
 ## randomForest
 library(randomForest)
 time.rf = system.time( {rf = randomForest(x, y)} )
-yhat    = predict(rf, newdata=x)
-yerr.rf = mean( (y-yhat)^2 )
+yhat    = predict(rf, newdata=xtest)
+yerr.rf = mean( (ytest-yhat)^2 )
 
 ## results:
 results = data.frame( 
