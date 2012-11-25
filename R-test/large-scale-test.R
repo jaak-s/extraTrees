1730868469fe83a09aff176de3e143c4cca5a92c
Jaak Simm <jaak.simm@gmail.com>
2012-11-25 12:23:55 +0900

    - fixed documentation, now passes R CMD check - added documentation for toJavaMatrix and toRMatrix

--- R-test/large-scale-test.R
+++ R-test/large-scale-test.R
@@ -2,11 +2,11 @@ library(extraTrees)
 library(randomForest)
 
 ## making data:
-n = 5000
+n = 2500
 p = 4
-x = matrix(runif(n*p), n, p)
 f = function(x) (x[,1]>0.5) + 0.8*(x[,2]>0.6) + 0.5*(x[,3]>0.4) + 0.1*runif(nrow(x))
 #f = function(x) (x[,1]>0.5) + 0.8*(x[,2]>0.6) + 0.5*(x[,3]>0.4) + 0.1*runif(nrow(x)) + x[,1]*0.4
+x = matrix(runif(n*p), n, p)
 y = as.numeric(f(x))
 xtest = matrix(runif(n*p), n, p)
 ytest = f(xtest)
@@ -14,7 +14,9 @@ ytest = f(xtest)
 methods = c(
     extraTrees.1c = function(x,y) extraTrees(x,y, nodesize=5, mtry=4),
     extraTrees.2c = function(x,y) extraTrees(x,y, nodesize=5, mtry=4, numRandomCuts=2),
+    extraTrees.2e = function(x,y) extraTrees(x,y, nodesize=5, mtry=4, numRandomCuts=2, evenCuts=TRUE),
     extraTrees.5c = function(x,y) extraTrees(x,y, nodesize=5, mtry=4, numRandomCuts=5),
+    extraTrees.5e = function(x,y) extraTrees(x,y, nodesize=5, mtry=4, numRandomCuts=5, evenCuts=TRUE),
     randomForest  = function(x,y) randomForest(x,y)
 )
 
