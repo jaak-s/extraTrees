6b0205bba2bac88bcf170258513767b93e1f5c3f
Jaak Simm <jaak.simm@gmail.com>
2012-11-28 16:25:03 +0900

    cleanup of prediction code

--- R-skeleton/R/predict.extraTrees.R
+++ R-skeleton/R/predict.extraTrees.R
@@ -7,7 +7,6 @@ predict.extraTrees <- function( object, newdata, ... )
     }
     et = object
         
-    #getValues(Matrix input)
     if (ncol(newdata)!=et$ndim) {
         stop( sprintf("newdata(ncol=%d) does not have the same dimensions as the original x (ncol=%d)", ncol(newdata), et$ndim) )
     }
@@ -18,6 +17,5 @@ predict.extraTrees <- function( object, newdata, ... )
     }
     ## classification:
     yhat = .jcall( et$jobject, "[I", "getValues", toJavaMatrix(newdata) )
-    #return(yhat)
     return( factor(yhat, levels=0:(length(et$levels)-1), labels=et$levels ) )
 }
