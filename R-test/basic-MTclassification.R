4f4b0c888ea0ff23e755823d2afeb74535e6a628
Jaak Simm <jaak.simm@gmail.com>
2013-10-05 15:33:20 +0300

    separated single-task learning

--- R-test/basic-MTclassification.R
+++ R-test/basic-MTclassification.R
@@ -24,14 +24,48 @@ makeData <- function(n = 1000, p = 10, ntasks = 50) {
 Dtrain = makeData()
 Dtest  = makeData()
 
+## single-task learning, all separated
+train.sep <- function(X, y, tasks) {
+    x = tapply( 1:nrow(X), tasks, function(i) {
+        ## training separate extraTrees
+        et = extraTrees( X[i,,drop=FALSE], y[i], nodesize=1, mTry=p, numRandomCuts=1 )
+        return(et)
+    })
+    return(x)
+}
+
+## prediction with separately learned trees
+predict.sep <- function(etsep, newdata, newtasks) {
+    res = sapply( 1:length(newtasks), function(i) {
+        task  = newtasks[i]
+        input = newdata[i,,drop=FALSE]
+        et    = etsep[[ task ]]
+        yhat  = predict(et, input)
+        return( yhat )
+    })
+    if (etsep[[1]]$factor) {
+        return( as.factor(res) )
+    }
+    return( res )
+}
+
+
 ## learning extra trees with multi-task learning:
-et = extraTrees(Dtrain$x, Dtrain$y, nodesize=1, mTry=p, numRandomCuts=10, tasks=Dtrain$tasks)
-et0 = extraTrees(Dtrain$x, Dtrain$y, nodesize=1, mTry=p, numRandomCuts=10)
+et = extraTrees(Dtrain$x, Dtrain$y, nodesize=1, mTry=p, numRandomCuts=1, tasks=Dtrain$tasks)
+et0 = extraTrees(Dtrain$x, Dtrain$y, nodesize=1, mTry=p, numRandomCuts=1)
+ets = train.sep(Dtrain$x, Dtrain$y, Dtrain$tasks)
+
 yhat = predict(et, Dtest$x, newtasks=Dtest$tasks)
 ## testing allValues
 yhatAll = predict(et, Dtest$x[1:10,], newtasks=Dtest$tasks[1:10], allValues=T)
 
+## pooled
 yhat0 = predict(et0, Dtest$x)
+## separated
+yhatSep = predict.sep(ets, Dtest$x, newtasks=Dtest$tasks)
+
 print( sprintf( "accuracy(extraTreesMT): %f", mean(Dtest$y==yhat) ) )
-print( sprintf( "accuracy(plain extraTrees): %f", mean(Dtest$y==yhat0) ) )
+print( sprintf( "accuracy(pooled extraTrees): %f", mean(Dtest$y==yhat0) ) )
+print( sprintf( "accuracy(separ. extraTrees): %f", mean(Dtest$y==yhatSep) ) )
+
 
