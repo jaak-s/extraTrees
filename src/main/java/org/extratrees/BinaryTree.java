9946d4ea0c82c5eb3da721d508ed81e22d6415e4
Jaak Simm <jaak.simm@gmail.com>
2014-05-17 01:15:20 +0200

    removed obsolete Tree functions

--- src/main/java/org/extratrees/BinaryTree.java
+++ src/main/java/org/extratrees/BinaryTree.java
@@ -9,43 +9,6 @@ public class BinaryTree extends AbstractBinaryTree<BinaryTree, Double> {
 		return AbstractTrees.NA;
 	}
 	
-	/**
-	 * Returns values for data points, each data point is a row of the matrix.
-	 * @param input
-	 * @return
-	 */
-	public double[] getValues(Matrix input) {
-		double[] values = new double[input.nrows()];
-		double[] temp = new double[input.ncols()];
-		for (int row=0; row<input.nrows(); row++) {
-			// copying matrix row to temp:
-			for (int col=0; col<input.ncols(); col++) {
-				temp[col] = input.get(row, col);
-			}
-			values[row] = getValue(temp);
-		}
-		return values;
-	}
-	
-	/**
-	 * uses nmin to choose the depth:
-	 * @param input
-	 * @param nmin
-	 * @return value in the tree for all <b>inputs</b> (each row in the matrix is an input).
-	 */
-	public double[] getValues(Matrix input, int nmin) {
-		double[] values = new double[input.nrows()];
-		double[] temp = new double[input.ncols()];
-		for (int row=0; row<input.nrows(); row++) {
-			// copying matrix row to temp:
-			for (int col=0; col<input.ncols(); col++) {
-				temp[col] = input.get(row, col);
-			}
-			values[row] = getValue(temp, nmin);
-		}
-		return values;
-	}
-	
 	/** count the number of times each column is used */
 	public int[] countColumns(int ncols) {
 		int[] counts = new int[ncols];
--- src/main/java/org/extratrees/FactorBinaryTree.java
+++ src/main/java/org/extratrees/FactorBinaryTree.java
@@ -9,43 +9,6 @@ public class FactorBinaryTree extends AbstractBinaryTree<FactorBinaryTree, Integ
 		return -1;
 	}
 
-	/**
-	 * Returns values for data points, each data point is a row of the matrix.
-	 * @param input
-	 * @return
-	 */
-	public int[] getValues(Matrix input) {
-		int[] values = new int[input.nrows()];
-		double[] temp = new double[input.ncols()];
-		for (int row=0; row < input.nrows(); row++) {
-			// copying matrix row to temp:
-			for (int col=0; col < input.ncols(); col++) {
-				temp[col] = input.get(row, col);
-			}
-			values[row] = getValue(temp);
-		}
-		return values;
-	}
-
-	/**
-	 * uses nmin to choose the depth:
-	 * @param input
-	 * @param nmin
-	 * @return value in the tree for all <b>inputs</b> (each row in the matrix is an input).
-	 */
-	public int[] getValues(Matrix input, int nmin) {
-		int[] values = new int[input.nrows()];
-		double[] temp = new double[input.ncols()];
-		for (int row=0; row < input.nrows(); row++) {
-			// copying matrix row to temp:
-			for (int col=0; col<input.ncols(); col++) {
-				temp[col] = input.get(row, col);
-			}
-			values[row] = getValue(temp, nmin);
-		}
-		return values;
-	}
-	
 	/** count the number of times each column is used */
 	public int[] countColumns(int ncols) {
 		int[] counts = new int[ncols];
