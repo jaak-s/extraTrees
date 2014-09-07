3bf929c096c2896196a7016ebb9bc997da750de6
Jaak Simm <jaak.simm@gmail.com>
2014-09-08 00:01:03 +0200

    random range performance

--- src/test/java/org/extratrees/BenchmarkRange.java
+++ src/test/java/org/extratrees/BenchmarkRange.java
@@ -1,5 +1,7 @@
 package org.extratrees;
 
+import java.util.ArrayList;
+
 import org.extratrees.data.Matrix;
 import org.junit.Test;
 
@@ -8,8 +10,8 @@ public class BenchmarkRange {
 	@Test
 	public void testRange() {
 		int N = 10*1000*1000;
-		int col = 2;
-		Matrix m = new Matrix(N, 5);
+		int col = 1;
+		Matrix m = new Matrix(N, 2);
 		for (int i=0; i<N; i++) {
 			m.set(i, col, Math.random());
 		}
@@ -17,7 +19,36 @@ public class BenchmarkRange {
 		double[] range;
 		Timer.tic();
 		range = AbstractTrees.getRange(ids, col, m);
-		Timer.toc("AbstractTrees.getRange");
+		Timer.toc("AbstractTrees.getRange(10M sequential)");
+	}
+	
+	public static int[] randIds(int size, int max) {
+		ArrayList<Integer> allIds = AbstractTrees.arrayToList( AbstractTrees.seq( max ) );
+		Timer.tic();
+		ShuffledIterator<Integer> shuffle = new ShuffledIterator<Integer>(allIds);
+		
+		int[] subset = new int[ size ];
+		for (int i=0; i < subset.length; i++) {
+			subset[i] = shuffle.next();
+		}
+		Timer.toc("shuffle");
+		return subset;
+	}
+	
+	@Test
+	public void testRangeOnRandom() {
+		int N = 1*1000*1000;
+		int col = 1;
+		Matrix m = new Matrix(N, 2);
+		for (int i=0; i<N; i++) {
+			m.set(i, col, Math.random());
+		}
+		
+		int[] ids = randIds(N/2, N);
+		double[] range;
+		Timer.tic();
+		range = AbstractTrees.getRange(ids, col, m);
+		Timer.toc("AbstractTrees.getRange(1M random-ids)");
 	}
 
 }
